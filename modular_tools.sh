0f152c37553ae7901928fb6384848aac5954f9a7
ilan.ganor <ilan@kamacode.com>
2024-01-21 10:59:01 +0200

    added modular tools

---
+++ modular_tools.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# source this script
+
+help()
+{
+	echo "build	- 	huild image"
+	echo "setup 	- 	one time build machine setup"
+	echo "help 	-	help menu"
+}
+
+setup()
+{
+	sudo apt-get -y install git git-doc subversion build-essential flex wget gawk unzip man file python2.7 zlib1g-dev libssl-dev libncurses5-dev
+}
+
+build()
+{
+	echo "build now"
+}
+
+view_build_details()
+{
+	echo "show image details"
+	leaf profile -v
+}
+
+if [ -n "$*" ]; then
+	eval "$*" # execute arguments
+	#echo $* finished, ret=$?
+else
+	if [ "$0" != "$BASH_SOURCE" ]; then
+		help
+	else
+		echo $BASH_SOURCE - a library of Sierra, mangOH, legato and yocto tools
+		help
+	fi
+fi
